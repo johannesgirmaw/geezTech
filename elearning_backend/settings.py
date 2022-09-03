c6ed3383b970aacebb4475e2b3ecb3ff157df9e0
Dires318 <getdera6591@gmail.com>
2022-09-03 12:31:37 +0300

    add commons and authentication module

--- elearning_backend/settings.py
+++ elearning_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commons.apps.CommonsConfig',
+    'commons.authentication.apps.AuthenticationConfig',
+
 ]
 
 MIDDLEWARE = [
