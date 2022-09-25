afe63ece9e1d67b0950d7932b8553e13e686e21e
yohannes girmaw <yg@etechsc.com>
2022-09-25 03:30:01 -0400

    custome user model added

--- commons/authentication/serializer.py
+++ commons/authentication/serializer.py
@@ -1,21 +1,21 @@
-# from rest_framework import serializers
-# from dj_rest_auth.registration.serializers import RegisterSerializer
-# from django.db import transaction
-# from .models import CustomUser
+from rest_framework import serializers
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from django.db import transaction
+from .models import CustomUser
 
-# class CustomRegisterSerializer(RegisterSerializer):
-#     first_name=serializers.CharField(max_length=200)
-#     last_name=serializers.CharField(max_length=200)
-#     # age=serializers.IntegerField()
-#     @transaction.atomic
-#     def save(self,request):
-#         user=super().save(request)
-#         user.first_name=self.data.get('first_name' )
-#         user.last_name=self.data.get('last_name' )
-#         # user.age=self.data.get('age')
-#         user.save()
-#         return user
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=CustomUser
-#         fields='__all__'
+class CustomRegisterSerializer(RegisterSerializer):
+    first_name=serializers.CharField(max_length=200)
+    last_name=serializers.CharField(max_length=200)
+    # age=serializers.IntegerField()
+    @transaction.atomic
+    def save(self,request):
+        user=super().save(request)
+        user.first_name=self.data.get('first_name' )
+        user.last_name=self.data.get('last_name' )
+        # user.age=self.data.get('age')
+        user.save()
+        return user
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CustomUser
+        fields='__all__'
