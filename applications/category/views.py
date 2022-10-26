628b34a05c867f74db51f5fac7e1489263de1fe7
Dires318 <getdera6591@gmail.com>
2022-10-26 19:47:51 +0300

    category crud

--- applications/category/views.py
+++ applications/category/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics,permissions
+from applications.category.models import Category
+from applications.category.serializers import CategorySerializer
 
 # Create your views here.
+class DetailCategory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+class ListCategory(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
