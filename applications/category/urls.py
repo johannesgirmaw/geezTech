e7155b036638d9bc5da5be6494e4ed2b972a0637
Dires <getdera6591@gmail.com>
2022-11-04 05:42:53 +0300

    template to pdf is intialized

--- applications/category/urls.py
+++ applications/category/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import DetailCategory, ListCategory
+from .views import DetailCategory, ListCategory, GeneratePdf
 
 urlpatterns = [
     path("<uuid:pk>/",DetailCategory.as_view()),
-    path("",ListCategory.as_view())
+    path("",ListCategory.as_view()),
+    path("generate/",GeneratePdf.as_view()),
 ]
--- applications/category/views.py
+++ applications/category/views.py
@@ -4,6 +4,10 @@ from applications.category.models import Category
 from applications.category.serializers import CategorySerializer
 from rest_framework import filters
 # Create your views here.
+from io import BytesIO
+from django.template.loader import get_template
+from django.http import HttpResponse
+from xhtml2pdf import pisa
 
 
 class DetailCategory(generics.RetrieveUpdateDestroyAPIView):
@@ -18,3 +22,24 @@ class ListCategory(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['category_name', 'description']
+
+class GeneratePdf(generics.ListAPIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+             'today': 12, 
+             'amount': 39.99,
+            'customer_name': 'Cooper Mann',
+            'order_id': 1233434,
+        }
+        pdf = render_to_pdf('invoice.html', data)
+        return HttpResponse(pdf, content_type='application/pdf')
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
