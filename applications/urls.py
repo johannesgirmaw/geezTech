6b4402c99f5874d99fdb94f93a589729e018f2c3
yohannes girmaw <yg@etechsc.com>
2022-11-16 02:44:30 -0500

    chapters crud

--- applications/urls.py
+++ applications/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include("applications.course.urls")),
-    path("category/", include("applications.category.urls")),
+    path('', include("applications.course.urls")),
+    path('', include("applications.category.urls")),
+    path('', include("applications.chapters.urls")),
 ]
