c18401129f675ab971163aab32ab330e1670f087
yohannes girmaw <yg@etechsc.com>
2022-12-10 18:39:20 +0300

    settings

--- elearning_backend/settings.py
+++ elearning_backend/settings.py
@@ -244,8 +244,8 @@ REGISTER_VERIFICATION_URL = "http://localhost:3000/user/activate"
 
 # Managing refresh tokens
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
